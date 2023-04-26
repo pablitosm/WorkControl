84edc565ee2b67376e4c9055efa67529d37e01b0
pablitosm <albertocipoteowo@gmail.com>
2023-04-26 10:01:32 +0200

    insercion en db

--- app/src/main/java/com/workcontrol/vistas/Inicio.java
+++ app/src/main/java/com/workcontrol/vistas/Inicio.java
@@ -1,5 +1,7 @@
 package com.workcontrol.vistas;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,18 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.workcontrol.R;
 
 public class Inicio extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,10 +34,35 @@ public class Inicio extends AppCompatActivity implements NavigationView.OnNaviga
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
 
+    FirebaseFirestore database;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        database = FirebaseFirestore.getInstance();
+        CollectionReference citiesRef = database.collection("Users");
+
+        database.collection("Users").whereEqualTo("contrasegna", "pepe")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " =>" + document.getData());
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     @Override
--- app/src/main/java/com/workcontrol/vistas/Registro.java
+++ app/src/main/java/com/workcontrol/vistas/Registro.java
@@ -3,19 +3,28 @@ package com.workcontrol.vistas;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.workcontrol.R;
+import com.workcontrol.modelo.Usuario;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Registro extends AppCompatActivity {
 
@@ -25,14 +34,17 @@ public class Registro extends AppCompatActivity {
     FirebaseDatabase nodoRaiz;
     DatabaseReference referencia;
 
+    FirebaseAuth auth;
+    FirebaseFirestore database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         buttonRegistrar = findViewById(R.id.buttonRegistro);
 
-        nodoRaiz = FirebaseDatabase.getInstance();
-        referencia = nodoRaiz.getReference();
+        database = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         TextoDNI = findViewById(R.id.editTextDNI);
         TextoNombre = findViewById(R.id.editTextNombre);
@@ -42,41 +54,53 @@ public class Registro extends AppCompatActivity {
 
         buttonRegistrar = findViewById(R.id.buttonRegistro);
 
+        // nodoRaiz = FirebaseDatabase.getInstance();
+        // referencia = nodoRaiz.getReference();
+
         buttonRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String dni = TextoDNI.getText().toString();
+                String nombre = TextoNombre.getText().toString();
+                String apellido = TextoApellido.getText().toString();
+                String correo = TextoCorreo.getText().toString();
+                String contrasegna = TextoContrasegna.getText().toString();
 
-                String getDNI = TextoDNI.getText().toString().trim();
-                String getNombre = TextoNombre.getText().toString().trim();
-                String getApellido = TextoApellido.getText().toString().trim();
-                String getCorreo = TextoCorreo.getText().toString().trim();
-                String getContrasegna = TextoContrasegna.getText().toString().trim();
+                auth.createUserWithEmailAndPassword(correo, contrasegna).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
 
-                HashMap<String, Object> mapica = new HashMap<>();
-                    mapica.put("DNI", getDNI);
-                    mapica.put("Nombre", getNombre);
-                    mapica.put("Apellido", getApellido);
-                    mapica.put("Correo", getCorreo);
-                    mapica.put("Contrasegna", getContrasegna);
+                        Map<String, String> datosUsuarios = new HashMap<>();
+                        datosUsuarios.put("dni", dni);
+                        datosUsuarios.put("nombre", nombre);
+                        datosUsuarios.put("apellido", apellido);
+                        datosUsuarios.put("correo", correo);
+                        datosUsuarios.put("contrasegna", contrasegna);
 
-                    referencia.child("users")
-                            .child(getNombre)
-                            .setValue(mapica)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void unused) {
-                                    Toast.makeText(getApplicationContext(), "Dato añadido correctamente", Toast.LENGTH_LONG).show();
+                        database.collection("Users").add(datosUsuarios).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getApplicationContext(), "EXITO", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(new Intent(Registro.this, Inicio.class));
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                            }
+                        });
 
-                                }
-                            })
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Registro.this, "ERROR", Toast.LENGTH_LONG).show();
+                    }
+                });
 
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(getApplicationContext(), "Error al añadir el dato", Toast.LENGTH_LONG).show();
-                                }
-                            });
+
             }
-        });
+            });
     }
 }
