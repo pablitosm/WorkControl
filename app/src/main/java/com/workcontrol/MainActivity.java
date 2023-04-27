e5243364b0120ceafe3130a118f94401dafa9ba8
pablitosm <albertocipoteowo@gmail.com>
2023-04-27 09:14:18 +0200

    boton registro

--- app/src/main/java/com/workcontrol/MainActivity.java
+++ app/src/main/java/com/workcontrol/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,36 +12,21 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.workcontrol.modelo.Usuario;
 import com.workcontrol.vistas.Inicio;
 import com.workcontrol.vistas.Registro;
 
-import java.io.FileInputStream;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<String> dni;
-    ArrayList<String> nombre;
-    ArrayList<String> apellido;
-    ArrayList<String> correo;
-    ArrayList<String> contrasegna;
 
     FirebaseAuth auth;
     FirebaseFirestore database;
 
     EditText textoUsuario;
     EditText textoContrasegna;
-
-    Usuario[] usuarioLogin = {new Usuario()};
 
     Button buttonLogin;
     Button buttonRegistro;
@@ -73,12 +57,22 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             startActivity(new Intent(MainActivity.this, Inicio.class));
+                        // hacer que detecte si el usuario existe pero la contraseña es incorrecta
+                            // (¡no funciona!)
+                        } else if (auth.fetchSignInMethodsForEmail(correoR) == null) {
+                            Toast.makeText(getApplicationContext(), "Contraseña incorrecta", Toast.LENGTH_LONG).show();
                         } else {
-                            Toast.makeText(getApplicationContext(), "Ese usuario no está registrado", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_LONG).show();
                         }
                     }
                 });
 
+            }
+        });
+        buttonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
             }
         });
     }
