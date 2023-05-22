5089c5874fa6adc6ff618bdc777a2e08ec5eca32
pablitosm <albertocipoteowo@gmail.com>
2023-05-22 16:20:43 +0200

    menu funcionando

--- app/src/main/java/com/workcontrol/vistas/InicioAdmin.java
+++ app/src/main/java/com/workcontrol/vistas/InicioAdmin.java
@@ -22,6 +22,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.navigation.NavigationView;
